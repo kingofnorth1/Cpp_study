19dcd528dba1fb7c1297ed3a82cc57ce62d29d47
kingofnorth1 <3441423164@qq.com>
2023-10-19 01:18:29 +0800

    23/10/19 leecode

--- cpp_arithmetic/Arithmetic/hello_algo_10.1.cpp
+++ cpp_arithmetic/Arithmetic/hello_algo_10.1.cpp
@@ -26,3 +26,21 @@ int binary_search(vector<int> &nums, int target) {
 	}
 	return -1;
 }
+
+int binarySearchInsertion(vector<int>& nums, int target) {
+	int i = 0, j = nums.size() - 1; // 初始化双闭区间 [0, n-1]
+	while (i <= j) {
+		int m = i + (j - i) / 2; // 计算中点索引 m
+		if (nums[m] < target) {
+			i = m + 1; // target 在区间 [m+1, j] 中
+		}
+		else if (nums[m] > target) {
+			j = m - 1; // target 在区间 [i, m-1] 中
+		}
+		else {
+			j = m - 1; // 首个小于 target 的元素在区间 [i, m-1] 中
+		}
+	}
+	// 返回插入点 i
+	return i;
+}
