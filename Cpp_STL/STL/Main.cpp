436aa275af8db80163d1b94d34b3679efa0462f4
杜乾坤 <3441423164@qq.com>
2023-10-20 17:44:16 +0800

    23/10/20 test

--- Cpp_STL/STL/Main.cpp
+++ Cpp_STL/STL/Main.cpp
@@ -1,6 +1,18 @@
 #include<iostream>
+#include<vector>
+#include<string>
 using namespace std;
+vector<string> demo();
 
 int main() {
-	cout << "test" << endl;
+	vector<string> out = demo();
+	for (auto i : out) {
+		cout << i << endl;
+	}
+}
+
+vector<string> demo() {
+	vector<string> res = { "abc","res" };
+	res[res.size() - 1] += "_";
+	return res;
 }
