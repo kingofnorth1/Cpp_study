3f28a37393ccf0f7293f9b0abc9b6e1fb50f8a41
杜乾坤 <3441423164@qq.com>
2023-11-10 17:57:17 +0800

    pick the peaks by codeWar

--- codeWar/codeWar/PickPeaks.cpp
+++ codeWar/codeWar/PickPeaks.cpp
@@ -6,13 +6,26 @@ struct PeakData {
 	vector<int> pos, peaks;
 };
 
-PeakData pickPeaks(const std::vector<int>& v) {
-	PeakData result;
-	for (int i = 1; i < v.size()-1;i++) {
-		if (v[i] > v[i - 1] && v[i] > v[i+1]) {
-			result.pos.push_back(i);
-			result.peaks.push_back(i);
-		}
-	}
-	return result;
+#include <vector>
+
+PeakData pick_peaks(const std::vector<int>& v) {
+    PeakData result;
+    for (int i = 1; i < v.size() - 1; i++) {
+        if (v[i] > v[i - 1] && v[i] > v[i + 1] && i != v.size() - 1) {
+            result.pos.push_back(i);
+            result.peaks.push_back(v[i]);
+        }
+        if (v[i] > v[i - 1] && v[i] == v[i + 1]) {
+            for (int j = i; j < v.size() - 1; j++) {
+                if (v[j] < v[j + 1]) {
+                    return result;
+                }
+                if (v[j] > v[j + 1]) {
+                    result.pos.push_back(i);
+                    result.peaks.push_back(v[i]);
+                }
+            }
+        }
+    }
+    return result;
 }
