bd0ae9947bed403c22a51285e0e1545bb36aa2e4
kingofnorth1 <3441423164@qq.com>
2023-11-13 00:58:36 +0800

    vcpkg安装opencv学习

--- codeWar/codeWar/MainWar.cpp
+++ codeWar/codeWar/MainWar.cpp
@@ -1,23 +1,23 @@
-#include<iostream>
-#include<vector>
-
-using namespace std;
-int romanToInt(string t1);
-string intToRoman(int t1);
-long sumTwoSmallestNumbers(vector<int> numbers);
-long sumTwoSmallestNumbersSort(vector<int> numbers);
-bool valid_braces(std::string braces);
-vector<vector<int>> multiplication_table(int t1);
-vector<int> pick_peaks(vector<int> v);
-
-int main() {
-	romanToInt("MDCLXVI");
-	intToRoman(3666);
-	vector<int> temp = { 3, 2, 3, 6, 4, 1, 2, 3, 2, 1, 2, 3 };
-	sumTwoSmallestNumbers(temp);			//通过定义firstMin和secondMin取得最小结果
-	sumTwoSmallestNumbersSort(temp);		//使用std::sort先进行排序，再将最小结果返回
-	cout << valid_braces("{}()") << endl;				//有效符号，{[()]}
-	multiplication_table(3);				//将int数转换为列表
-	pick_peaks(temp);						//选择数组的峰值
-	return 1;
-}
+//#include<iostream>
+//#include<vector>
+//
+//using namespace std;
+//int romanToInt(string t1);
+//string intToRoman(int t1);
+//long sumTwoSmallestNumbers(vector<int> numbers);
+//long sumTwoSmallestNumbersSort(vector<int> numbers);
+//bool valid_braces(std::string braces);
+//vector<vector<int>> multiplication_table(int t1);
+//vector<int> pick_peaks(vector<int> v);
+//
+//int main() {
+//	romanToInt("MDCLXVI");
+//	intToRoman(3666);
+//	vector<int> temp = { 3, 2, 3, 6, 4, 1, 2, 3, 2, 1, 2, 3 };
+//	sumTwoSmallestNumbers(temp);			//通过定义firstMin和secondMin取得最小结果
+//	sumTwoSmallestNumbersSort(temp);		//使用std::sort先进行排序，再将最小结果返回
+//	cout << valid_braces("{}()") << endl;				//有效符号，{[()]}
+//	multiplication_table(3);				//将int数转换为列表
+//	pick_peaks(temp);						//选择数组的峰值
+//	return 1;
+//}
---
+++ codeWar/codeWar/opencvTest.cpp
@@ -0,0 +1,19 @@
+#include <opencv2/opencv.hpp>
+using namespace cv;// 省去函数前面加cv::的必要性
+
+int main()
+{
+    char imageName[] = "C:/Users/king/Pictures/test.jpg";
+    Mat M = imread(imageName, IMREAD_COLOR);   // 读入图片 
+
+    if (M.empty())     // 判断文件是否正常打开  
+    {
+        fprintf(stderr, "Can not load image %s\n", imageName);
+        waitKey(6000);  // 等待6000 ms后窗口自动关闭   
+        return -1;
+    }
+
+    imshow("image", M);  // 显示图片 
+    waitKey();
+    return 0;
+}
